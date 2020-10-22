8997f36a0417f25296d32f59e79f1f137db9de08
Dominique De La Cruz <dndlc8@gmail.com>
2020-10-21 23:26:17 -0700

    make search engine functional

--- src/Weather.js
+++ src/Weather.js
@@ -1,15 +1,17 @@
 import React, {useState} from 'react';
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 import './Weather.css';
 
 
 export default function Weather (props) {
   const [forecast, setForecast]= useState ({ready: false});
+  const [city, setCity]= useState(props.defaultCity);
 
   function handleResponse(response) {
     setForecast({
       ready: true,
+      city: response.data.name,
       date: new Date(response.data.dt * 1000),
       temperature: Math.round(response.data.main.temp),
       description: response.data.weather[0].description,
@@ -22,76 +24,39 @@ export default function Weather (props) {
     })
   }
 
+  function search() {
+    let units = "imperial";
+    let apiKey = "1b9a19801a7a96280358cc2498e9820b";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+    }
+
+  function handleChange (event) {
+    setCity(event.target.value);
+  }
+  
+
 
   if (forecast.ready){
   return (
     <div className="weather">
-      <form className="search">
+      <form className="search" onSubmit={handleSubmit}>
         <input  type="search" placeholder="Enter A City"
-        className="formControl" />
+        className="formControl" onChange={handleChange} />
         <input type ="submit" value="Search" className="btn btn-dark"/>
       </form>
-      <h1 className="cityHeader">{props.defaultCity}
-      </h1>
-      <h3 className="date"><FormattedDate date={forecast.date} /></h3>
-      <div className="currentContainer row">
-       <img className="icon col-5" src={forecast.iconUrl} alt={forecast.description}/>
-      <div className="current"><span className="currentTemp">{forecast.temperature}</span> 
-        <span>°F</span><br /><span className="weatherDescription text-capitalize">{forecast.description} </span>
-      </div>
-     
-       </div>
-      
-      <table className="weatherInfo">
-        <tr>
-          <td className="tableCategory feelsCater">
-          Feels Like
-          </td>
-          <td className="tableCategory hiCater">
-          Hi
-          </td>
-          <td className="tableCategory lowCater">
-          Low
-          </td>
-        </tr>
-        <tr>
-          <td className= "info feelsLike">
-          <span className="feelNumber">{forecast.feels}</span>°F
-          </td>
-          <td className= "info tempHiInfo">
-          <span className="hiNumber">{forecast.hiTemp}</span>°F
-          </td>
-          <td className="info tempLowInfo">
-          <span className="lowNumber">{forecast.lowTemp}</span>°F
-          </td>
-        </tr>
-        <tr>
-          <td className="tableCategory windCater">
-          Wind
-          </td>
-          <td className="tableCategory humidCater">
-          Humidity
-          </td>
-        </tr>
-        <tr>
-          <td className="info windInfo">
-          <span className="windNumber">{forecast.wind}{" "}</span><span className="windMeasure">mph</span>
-          </td>
-          <td className="info humidityInfo">
-          <span className="humidNumber">{forecast.humidity}</span>%
-          </td>
-        </tr>
-      </table>
+      <WeatherInfo data={forecast}/>
 
     </div>
 
   );
   } else {
-  let units = "imperial";
-  let apiKey = "1b9a19801a7a96280358cc2498e9820b";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(handleResponse);
-
+    search();
   return "Check Back Later"
   }
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,63 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherInfo(props) {
+  return (
+  <div className="weatherInfo">
+  
+      <h3 className="date"><FormattedDate date={props.data.date} /></h3>
+      <h1 className="cityHeader">{props.data.city}
+      </h1>
+      <div className="currentContainer row">
+       <img className="icon col-5" src={props.data.iconUrl} alt={props.data.description}/>
+      <div className="current"><span className="currentTemp">{props.data.temperature}</span> 
+        <span>°F</span><br /><span className="weatherDescription text-capitalize">{props.data.description} </span>
+      </div>
+     
+       </div>
+      
+      <table className="weatherInfo">
+        <tbody>
+        <tr>
+          <td className="tableCategory feelsCater">
+          Feels Like
+          </td>
+          <td className="tableCategory hiCater">
+          Hi
+          </td>
+          <td className="tableCategory lowCater">
+          Low
+          </td>
+        </tr>
+        <tr>
+          <td className= "info feelsLike">
+          <span className="feelNumber">{props.data.feels}</span>°F
+          </td>
+          <td className= "info tempHiInfo">
+          <span className="hiNumber">{props.data.hiTemp}</span>°F
+          </td>
+          <td className="info tempLowInfo">
+          <span className="lowNumber">{props.data.lowTemp}</span>°F
+          </td>
+        </tr>
+        <tr>
+          <td className="tableCategory windCater">
+          Wind
+          </td>
+          <td className="tableCategory humidCater">
+          Humidity
+          </td>
+        </tr>
+        <tr>
+          <td className="info windInfo">
+          <span className="windNumber">{props.data.wind}{" "}</span><span className="windMeasure">mph</span>
+          </td>
+          <td className="info humidityInfo">
+          <span className="humidNumber">{props.data.humidity}</span>%
+          </td>
+        </tr>
+        </tbody>
+      </table>
+      </div>
+  );
+}
